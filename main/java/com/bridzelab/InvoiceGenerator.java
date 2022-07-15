ecfd8efc566fa22355853c25b334f08d33da571b
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-15 21:16:59 +0530

    Added : given dis and time find the total fare

--- main/java/com/bridzelab/InvoiceGenerator.java
+++ main/java/com/bridzelab/InvoiceGenerator.java
@@ -1,8 +1,13 @@
 package com.bridzelab;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Cab Invoice Generator Program");
+
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return totalFare;
     }
 
 }
