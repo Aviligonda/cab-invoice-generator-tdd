c178ed35f279455024297829cb4390f260675933
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-15 21:20:13 +0530

    Refatcor : Should retrun minimum fare

--- test/java/com/bridzelab/InvoiceGeneratorTest.java
+++ test/java/com/bridzelab/InvoiceGeneratorTest.java
@@ -12,4 +12,14 @@ public class InvoiceGeneratorTest {
         double fare = generator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceGenerator generator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = generator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+
+    }
 }
